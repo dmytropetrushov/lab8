f42b0f9e6c81ab7529e7df1885ec4837387623f0
dmytropetrushov <114195363+dmytropetrushov@users.noreply.github.com>
2023-06-07 01:03:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,218 +1,90 @@
-import nltk
+import tkinter as tk
 from nltk.corpus import wordnet as wn
 
-nltk.download('wordnet')
+# Create the main application window
+root = tk.Tk()
+root.title("NLTK Word Tasks")
+root.geometry("400x300")
 
-# Task 1
-synsets = wn.synsets('dish')
-print("==All synsets for 'dish'==")
-for synset in synsets:
-    print(f"{synset.name()}: {synset.definition()}")
+# Create a frame to contain the tasks
+tasks_frame = tk.Frame(root, padx=20, pady=20)
+tasks_frame.pack()
+# Task 1: Get all synsets for the word "dish"
+def task1():
+    synsets = wn.synsets("dish")
+    result_text = '\n'.join([str(synset) for synset in synsets])
+    show_result("Task 1", result_text)
 
-# Task 2
-print("\n==Retrieve all noun and verb synsets for 'dish'==")
-noun_synsets = wn.synsets('dish', pos=wn.NOUN)
-verb_synsets = wn.synsets('dish', pos=wn.VERB)
-
-# Print noun synsets with definitions and examples
-print("\nNoun Synsets:")
-for i, synset in enumerate(noun_synsets):
-    print(f"{i + 1}. Definition: {synset.definition()}")
-    examples = synset.examples()
-    if examples:
-        print(f"   Example: {examples[0]}")
+# Task 2: Get a definition and example using the lemma of the third noun synset
+# and the first verb synset of the word "dish"
+# Task 2: Get a definition and example using the lemma of the third noun synset
+# and the first verb synset of the word "dish"
+def task2():
+    noun_synsets = wn.synsets("dish", pos=wn.NOUN)
+    verb_synsets = wn.synsets("dish", pos=wn.VERB)
+    if len(noun_synsets) > 2 and len(verb_synsets) > 0:
+        noun_lemma = noun_synsets[2].lemmas()[0]
+        verb_lemma = verb_synsets[0].lemmas()[0]
+        noun_synset = noun_lemma.synset()
+        verb_synset = verb_lemma.synset()
+        result_text = f"Definition: {noun_synset.definition()}\nExample: {verb_synset.examples()[0]}"
+        show_result("Task 2", result_text)
     else:
-        print("   No examples available.")
+        show_result("Task 2", "Insufficient synsets found")
 
-# Print verb synsets with definitions and examples
-print("\nVerb Synsets:")
-for i, synset in enumerate(verb_synsets):
-    print(f"{i + 1}. Definition: {synset.definition()}")
-    examples = synset.examples()
-    if examples:
-        print(f"   Example: {examples[0]}")
+
+# Task 3: Get all names, definitions, and examples of uses of the noun "dish" synsets
+def task3():
+    noun_synsets = wn.synsets("dish", pos=wn.NOUN)
+    result_text = ""
+    for synset in noun_synsets:
+        result_text += f"Name: {synset.name()}\nDefinition: {synset.definition()}\n"
+        if synset.examples():
+            result_text += f"Example: {synset.examples()[0]}\n"
+        result_text += "\n"
+    show_result("Task 3", result_text)
+
+# Task 4: Get all names, definitions, and examples of verb synsets "dish"
+def task4():
+    verb_synsets = wn.synsets("dish", pos=wn.VERB)
+    result_text = ""
+    for synset in verb_synsets:
+        result_text += f"Name: {synset.name()}\nDefinition: {synset.definition()}\n"
+        if synset.examples():
+            result_text += f"Example: {synset.examples()[0]}\n"
+        result_text += "\n"
+    show_result("Task 4", result_text)
+
+# Task 5: Get the lemma and the name of this lemma of any synset of the word "dish"
+def task5():
+    synsets = wn.synsets("dish")
+    if len(synsets) > 0:
+        lemma = synsets[0].lemmas()[0]
+        result_text = f"Lemma: {lemma}\nLemma name: {lemma.name()}"
+        show_result("Task 5", result_text)
     else:
-        print("   No examples available.")
+        show_result("Task 5", "No synsets found")
 
-# Task 3
-print('== Synsets for the noun \'dish\' ==\n')
-noun_synsets = wn.synsets('dish', pos=wn.NOUN)
+# Helper function to display the result in a new window
+def show_result(task_name, result_text):
+    result_window = tk.Toplevel(root)
+    result_window.title(task_name)
+    result_label = tk.Label(result_window, text=result_text)
+    result_label.pack()
 
-# Print names, definitions, and examples of each synset
-for synset in noun_synsets:
-    print(f"Name: {synset.name()}")
-    print(f"Definition: {synset.definition()}")
+# Create buttons for each task
+task1_button = tk.Button(root, text="Task 1", command=task1)
+task2_button = tk.Button(root, text="Task 2", command=task2)
+task3_button = tk.Button(root, text="Task 3", command=task3)
+task4_button = tk.Button(root, text="Task 4", command=task4)
+task5_button = tk.Button(root, text="Task 5", command=task5)
 
-    # Print examples, if any
-    examples = synset.examples()
-    if examples:
-        print("Examples:")
-        for example in examples:
-            print(f"- {example}")
-    else:
-        print("No examples found.")
+# Position the buttons in the main window
+task1_button.pack()
+task2_button.pack()
+task3_button.pack()
+task4_button.pack()
+task5_button.pack()
 
-# Task 4
-print('\n\n==Print names, definitions, and examples of each synset for the \'dish\' verb==')
-verb_synsets = wn.synsets('dish', pos=wn.VERB)
-
-# Print names, definitions, and examples of each synset
-for synset in verb_synsets:
-    print(f"Name: {synset.name()}")
-    print(f"Definition: {synset.definition()}")
-    examples = synset.examples()
-    if examples:
-        print("Examples:")
-        for example in examples:
-            print(f"- {example}")
-    else:
-        print("No examples found.")
-    print()
-
-# Task 5
-print('\n\n==Get the lemma and the given lemma name of any synset of the word "dish"==')
-
-synsets = wn.synsets('dish')
-synset = synsets[0]
-
-lemma = synset.lemmas()[0]
-lemma_name = lemma.name()
-
-print("Lemma name:", lemma_name)
-print("Synset definition:", synset.definition())
-
-# Task 6
-
-print("\n==Get all lower and higher concepts in the WordNet generic hierarchy of the first synset of the noun 'work'. "
-      "Get the root hypernym of this concept. ==")
-
-# Get the first synset for the noun "work"
-work_synset = wn.synset('work.n.01')
-
-# Get all the hyponyms (lower concepts) of the work synset
-hyponyms = work_synset.hyponyms()
-
-print("\nLower concepts of 'work.n.01':")
-for hyponym in hyponyms:
-    print(f"- Name: {hyponym.name()}")
-    print(f"  Definition: {hyponym.definition()}")
-
-# Get all the hypernyms (higher concepts) of the work synset
-hypernyms = work_synset.hypernyms()
-
-print("\nHigher concepts of 'work.n.01':")
-for hypernym in hypernyms:
-    print(f"- Name: {hypernym.name()}")
-    print(f"  Definition: {hypernym.definition()}")
-
-# Get the root hypernym of the work synset
-root_hypernym = work_synset.root_hypernyms()
-
-print("\nRoot hypernym of 'work.n.01':")
-print(f"- Name: {root_hypernym[0].name()}")
-print(f"  Definition: {root_hypernym[0].definition()}")
-
-# Task 7
-print("\n==Use WordNet to find out what substances 'wood is made of (use the first lemma of the noun) and what "
-      "substances it is part of==")
-
-# Get the first noun synset for "wood"
-wood_synset = wn.synsets('wood', pos=wn.NOUN)[0]
-
-# Get the hyponyms for "wood"
-wood_hyponyms = wood_synset.hyponyms()
-print("\nWood Hyponyms:")
-for hyponym in wood_hyponyms:
-    print(" - " + hyponym.name().split(".")[0])
-
-# Get the substance meronyms for "wood"
-wood_substance_meronyms = wood_synset.substance_meronyms()
-print("\nWood Substance Meronyms:")
-for meronym in wood_substance_meronyms:
-    print(" - " + meronym.name().split(".")[0])
-
-# Task 8
-print("\n==Find an antonym for 'horizontal'==")
-# get all synsets of the adjective "horizontal"
-synsets = wn.synsets('horizontal', pos=wn.ADJ)
-
-# iterate over all the synsets and their lemmas to find antonyms
-for synset in synsets:
-    for lemma in synset.lemmas():
-        if lemma.antonyms():
-            print(f"Antonym of 'horizontal': {lemma.antonyms()[0].name()}")
-            break
-
-# Task 9
-print("\n==Show what actions are involved in the concept of 'eat'==")
-synsets = wn.synsets('eat')
-verb_synsets = [synset for synset in synsets if synset.pos() == 'v']
-for synset in verb_synsets:
-    print(f"{synset.name()}: {synset.definition()}")
-    hypernyms = synset.hypernyms()
-    hyponyms = synset.hyponyms()
-    print("  Hypernyms:")
-    for hypernym in hypernyms:
-        print(f"    - {hypernym.name()}: {hypernym.definition()}")
-    print("  Hyponyms:")
-    for hyponym in hyponyms:
-        print(f"    - {hyponym.name()}: {hyponym.definition()}")
-
-# Task 10
-print("\n==Get the synsets for 'bowl.n.02' and 'polyhedron.n.01'==")
-bowl = wn.synset('bowl.n.02')
-polyhedron = wn.synset('polyhedron.n.01')
-
-# Get the lowest common hypernyms of the two synsets
-common_hypernyms = bowl.lowest_common_hypernyms(polyhedron)
-
-# Print the name and definition of the smallest common hyponym
-if common_hypernyms:
-    smallest_common_hyponym = common_hypernyms[0]
-    print("Smallest common hyponym:", smallest_common_hyponym.name())
-    print("Definition:", smallest_common_hyponym.definition())
-else:
-    print("No common hyponyms found.")
-
-# Task 11
-print("\n==Get the synsets for 'bowl.n.01' and 'polyhedron.n.01'==")
-bowl = wn.synset('bowl.n.02')
-polyhedron = wn.synset('polyhedron.n.01')
-
-# Get the lowest common hypernyms of the two synsets
-common_hypernyms = bowl.lowest_common_hypernyms(polyhedron)
-
-# Print the name and definition of the smallest common hyponym
-if common_hypernyms:
-    smallest_common_hyponym = common_hypernyms[0]
-    print("Smallest common hyponym:", smallest_common_hyponym.name())
-    print("Definition:", smallest_common_hyponym.definition())
-else:
-    print("No common hyponyms found.")
-
-# Task 13
-print("\n==Determine the semantic distance between the job.n.07 and work.n.01 synsets==")
-job_synset = wn.synset('job.n.07')
-work_synset = wn.synset('work.n.01')
-
-distance = job_synset.path_similarity(work_synset)
-
-print("Semantic distance between 'job.n.07' and 'work.n.01':", distance)
-
-# Task 14
-print("\n==Determine the semantic distance between the job.n.07 and work.n.07 synsets==")
-job_synset = wn.synset('job.n.07')
-work_synset = wn.synset('work.n.07')
-
-distance = job_synset.path_similarity(work_synset)
-
-print("Semantic distance between 'job.n.07' and 'work.n.07':", distance)
-
-# Task 15
-print("\n==Determine the semantic distance between the job.n.07 and entity.n.01 synsets==")
-job_synset = wn.synset('job.n.07')
-work_synset = wn.synset('entity.n.01')
-
-distance = job_synset.path_similarity(work_synset)
-
-print("Semantic distance between 'job.n.07' and 'entity.n.01':", distance)
+# Start the main GUI event loop
+root.mainloop()
